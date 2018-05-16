d047c0021b7a54e4ec916317b774849d0e85383d
sivadevi.ch <sivadevi.ch@gmail.com>
2018-05-16 17:14:11 -0400

    First commit

---
+++ src/main/java/com/myfirst/maven/quickstart/App.java
@@ -0,0 +1,16 @@
+package com.myfirst.maven.quickstart;
+
+import com.google.gson.Gson;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	 Gson gson = new Gson();
+         System.out.println(gson.toJson("Hello World!") );
+    }
+}
