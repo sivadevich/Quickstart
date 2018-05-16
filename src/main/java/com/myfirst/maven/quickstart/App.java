315329d1f8a6ceced1046dfd8123e576f4bac3e0
sivadevich <sivadevi.ch@gmail.com>
2018-05-16 17:37:37 -0400

    Update App.java

--- src/main/java/com/myfirst/maven/quickstart/App.java
+++ src/main/java/com/myfirst/maven/quickstart/App.java
@@ -10,7 +10,7 @@ public class App
 {
     public static void main( String[] args )
     {
-    	 Gson gson = new Gson();
+    	 Gson gson = new Gson()
          System.out.println(gson.toJson("Hello World!") );
     }
 }
